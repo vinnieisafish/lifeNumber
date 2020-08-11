7ee2ff440a1b1932e66c692b0dc66bfb928ec110
Vinnie <rmp841209@gmail.com>
2020-08-11 20:52:55 +0800

    Initial Commit

---
+++ lifeNumber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lifeNumber
+//
+//  Created by 林盈君 on 2020/8/11.
+//  Copyright © 2020 林盈君. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
